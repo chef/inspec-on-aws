a1321ed70cdf1281858e54e4ef6443a953f83a1a
Thomas Petchel <tpetchel@gmail.com>
2018-03-05 23:38:55 -0500

    Refactor

--- main.tf
+++ main.tf
@@ -1,30 +1,183 @@
 #
-# VARIABLES
+# Variables
 #
 
-variable "aws_region" {
-  description = "AWS region to launch servers."
-  # This value is being hard-coded to this region. I believe it could be specified at run-time or at some other point. I haven't spent much time with the variables yet to know all the details.
-  default     = "us-east-1"
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" { default = "us-east-1" }
+variable "aws_availability_zone" { default = "a" }
+
+#
+# Provider
+#
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
 }
 
-variable "aws_amis" {
-  default = {
-    # This variable is used later for the lookup based on the region value. This is useful if you want to build things in different regions. This pattern was in the already existing examples.
-    # Chef Essentials 7.0.0
-    us-east-1 = "ami-97785bed"
+#
+# Data
+#
+
+# This retrieves the latest AMI ID for Ubuntu 16.04.
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
-variable "aws_availability_zone" {
-    # There were some errors about the subnet being created in an availability
-    # zone that did not have access to t1.micros.
-    default = "us-east-1a"
+#
+# Creation
+#
+
+# networking
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.100.0/24"
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+}
+
+resource "aws_security_group" "ssh" {
+  name = "learn_chef_ssh"
+  description = "Used in a terraform exercise"
+  vpc_id = "${aws_vpc.default.id}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web" {
+  name = "learn_chef_web"
+  description = "Used in a terraform exercise"
+  vpc_id = "${aws_vpc.default.id}"
+
+  # Allow inbound HTTP connection from all
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "mysql" {
+  name = "learn_chef_mysql"
+  description = "Used in a terraform exercise"
+  vpc_id = "${aws_vpc.default.id}"
+
+  # Allow inbound TCP connection for MySql from instances from the public subnet
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  # Allow inbound TCP connection for MySql from instances from the private subnet
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["10.0.100.0/24"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# instances
+
+resource "aws_instance" "webserver" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.web.id}"]
+  subnet_id = "${aws_subnet.public.id}"
+  tags {
+    Name = "webserver"
+  }
+}
+
+resource "aws_instance" "database" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  availability_zone = "${var.aws_region}${var.aws_availability_zone}"
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.mysql.id}"]
+  subnet_id = "${aws_subnet.private.id}"
+  tags {
+    Name = "database"
+  }
 }
 
 #
-# OUTPUT
+# Output
 #
+
+# AMI ID
+
+output "image_id" {
+  value = "${data.aws_ami.ubuntu.id}"
+}
 
 # webserver details
 
@@ -98,136 +251,4 @@ output "security_group.ssh.id" {
 
 output "route.internet_access.id" {
   value = "${aws_route.internet_access.route_table_id}"
-}
-
-#
-# Creation
-#
-
-# instances
-
-resource "aws_instance" "webserver" {
-  ami           = "${lookup(var.aws_amis, var.aws_region)}"
-  instance_type = "t1.micro"
-  vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.web.id}"]
-  subnet_id = "${aws_subnet.public.id}"
-  availability_zone = "${var.aws_availability_zone}"
-  tags {
-    Name = "webserver"
-  }
-}
-
-
-resource "aws_instance" "database" {
-  ami           = "${lookup(var.aws_amis, var.aws_region)}"
-  instance_type = "t1.micro"
-  vpc_security_group_ids = ["${aws_security_group.ssh.id}", "${aws_security_group.mysql.id}"]
-  subnet_id = "${aws_subnet.private.id}"
-  availability_zone = "${var.aws_availability_zone}"
-  tags {
-    Name = "database"
-  }
-}
-
-# networking
-
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = "${aws_vpc.default.id}"
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "${var.aws_availability_zone}"
-}
-
-resource "aws_subnet" "private" {
-  vpc_id                  = "${aws_vpc.default.id}"
-  cidr_block              = "10.0.100.0/24"
-  availability_zone = "${var.aws_availability_zone}"
-}
-
-resource "aws_security_group" "ssh" {
-  name        = "learn_chef_ssh"
-  description = "Used in a terraform exercise"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "web" {
-  name        = "learn_chef_web"
-  description = "Used in a terraform exercise"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  # Allow inbound HTTP connection from all
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "mysql" {
-  name        = "learn_chef_mysql"
-  description = "Used in a terraform exercise"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  # Allow inbound TCP connection for MySql from instances from the public subnet
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/24"]
-  }
-
-  # Allow inbound TCP connection for MySql from instances from the private subnet
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.100.0/24"]
-  }
-
-  # outbound internet access
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
